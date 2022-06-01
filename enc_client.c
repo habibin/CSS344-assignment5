ff04762c9a84b0be81d8677f376800bd3ddeb632
habibin <habibin@oregonstate.edu>
2022-06-01 01:14:25 -0700

    finished some

--- enc_client.c
+++ enc_client.c
@@ -50,6 +50,7 @@ void setupAddressStruct(struct sockaddr_in* address, int portNumber){
 
 
 char* read_func(char* textfile){
+    char characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
     //reads the file and puts it into the buffer
  
     FILE* fp = fopen(textfile, "r"); //opens file
@@ -67,8 +68,16 @@ char* read_func(char* textfile){
     rewind(fp);
     //create a buffer with filesize memory
     char*buffer = (char*)malloc(sizeof(char) * (filesize + 1));
-    //reads the file into the buffer
-    fread(buffer, sizeof(char), filesize, fp);
+    //verifies each character then puts it into the buffer
+    int i = 0;
+    while (i < filesize){
+        buffer[i] = fgetc(fp);
+        if (buffer[i] != '\n' && strchr(characters, buffer[i]) == NULL){
+            fprintf(stderr, RED "unapproved character is found in file");
+            exit(1);
+        }
+        i++;
+    }
     //closes file
     fclose(fp);
     
@@ -91,6 +100,13 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+
+    //converts text & key filessizes from number to string
+    char num_str1[10];
+    char num_str2[10];
+    sprintf(num_str1,"%d", strlen(text_buffer));
+    sprintf(num_str2,"%d", strlen(key_buffer));
+
     // Create a socket
     int socketFD = socket(AF_INET, SOCK_STREAM, 0); 
     if (socketFD < 0){
@@ -107,18 +123,67 @@ int main(int argc, char *argv[]) {
     }
 
     // Send message to server to identify itself
-    char message[1] = "$";
-    char handshake = send(socketFD, message, strlen(message), 0); 
+    int handshake = send(socketFD, "?\0", 2, 0); 
     if (handshake < 0){
         fprintf(stderr, "CLIENT: ERROR writing to socket");
     }
-    if (handshake < strlen(message)){
-        printf("CLIENT: WARNING: Not all data written to socket!\n");
+
+    // Get handshake message from server
+    char handshake_buff[3];
+    int charsRead = recv(socketFD, handshake_buff, 2, 0); 
+    if (charsRead < 0){
+        error("CLIENT: ERROR reading from socket");
+    }
+    printf("CLIENT: I received this from the server: %s\n", handshake_buff);
+
+    //verifies handshake message
+    //if message matches, continue
+    if (strcmp(handshake_buff,"@\0") == 0){
+        //sends the file size of textfile to server
+        int filesize1 = send(socketFD, num_str1 , strlen(num_str1), 0); 
+        if (filesize1 < 0){
+            fprintf(stderr, "CLIENT: ERROR writing to socket");
+        }
+
+       // Get confirmation for filesize from server
+        char confirm_buf[3];
+        int charsRead1 = recv(socketFD, confirm_buf, 2, 0); 
+        if (charsRead1 < 0){
+            error("CLIENT: ERROR reading from socket");
+        }
+        printf("CLIENT: I received this from the server: %s\n", confirm_buf); 
+
+        //sends the textfile to server
+        int counter = 0; // keeps track of how many bytes have been sent
+        while (1){
+            int textfile = send(socketFD, text_buffer , strlen(text_buffer), 0); 
+            if (textfile < 0){
+                fprintf(stderr, "CLIENT: ERROR writing to socket");
+            }
+            //checks to see if all file has been sent
+            counter = textfile + counter;
+            printf("textfile: %d counter: %d strlen: %d\n", textfile,counter, strlen(text_buffer));
+            if (counter == strlen(text_buffer)){
+                break;
+            }
+        }
+        // //loops until entire file has been sent
+        // while(1){
+        //     // Sends concatenized text and key file to server
+        //     int handshake = send(socketFD, cat_buff , strlen(cat_buff), 0); 
+        //     if (handshake < 0){
+        //         fprintf(stderr, "CLIENT: ERROR writing to socket");
+        //     }
+        // }
+
+        close(socketFD);
     }
 
-
-    // Close the socket
-    close(socketFD); 
+    //else it closes the socket connection
+    else{
+        // Close the socket
+        close(socketFD); 
+    }
     return 0;
     
 }
--- enc_server.c
+++ enc_server.c
@@ -5,85 +5,132 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#define RED "\e[0;31m"  //color red is defined
+#define RED "\e[0;31m"  //color red is definedI
+#define WHITE "\e[0;37m"
 
 // Error function used for reporting issues
 void error(const char *msg) {
-perror(msg);
-exit(1);
+    perror(msg);
+    exit(1);
 } 
 
 // Set up the address struct for the server socket
 void setupAddressStruct(struct sockaddr_in* address, int portNumber){
 
-// Clear out the address struct
-memset((char*) address, '\0', sizeof(*address)); 
+    // Clear out the address struct
+    memset((char*) address, '\0', sizeof(*address)); 
 
-// The address should be network capable
-address->sin_family = AF_INET;
-// Store the port number
-address->sin_port = htons(portNumber);
-// Allow a client at any address to connect to this server
-address->sin_addr.s_addr = INADDR_ANY;
+    // The address should be network capable
+    address->sin_family = AF_INET;
+    // Store the port number
+    address->sin_port = htons(portNumber);
+    // Allow a client at any address to connect to this server
+    address->sin_addr.s_addr = INADDR_ANY;
 }
 
 
 
 int main(int argc, char *argv[]){
-int connectionSocket;
+    int connectionSocket;
 
-struct sockaddr_in serverAddress, clientAddress;
-socklen_t sizeOfClientInfo = sizeof(clientAddress);
+    struct sockaddr_in serverAddress, clientAddress;
+    socklen_t sizeOfClientInfo = sizeof(clientAddress);
 
 
-// Create the socket that will listen for connections
-int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
-if (listenSocket < 0) {
-    fprintf(stderr, RED "ERROR opening socket");
-}
-
-// Set up the address struct for the server socket
-setupAddressStruct(&serverAddress, atoi(argv[1]));
-
-// Associate the socket to the port
-int binding_status = bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
-if (binding_status < 0){
-    fprintf(stderr, RED "ERROR on binding");
-    exit(1);
-}
-
-// Start listening for connetions. Allow up to 5 connections to queue up
-listen(listenSocket, 5); 
-
-// Accept a connection, blocking if one is not available until one connects
-while(1){
-    // Accept the connection request which creates a connection socket
-    connectionSocket = accept(listenSocket, 
-                (struct sockaddr *)&clientAddress, 
-                &sizeOfClientInfo); 
-    if (connectionSocket < 0){
-        error("ERROR on binding");
+    // Create the socket that will listen for connections
+    int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket < 0) {
+        fprintf(stderr, RED "ERROR opening socket");
     }
 
-    printf("SERVER: Connected to client running at host %d port %d\n", 
-                        ntohs(clientAddress.sin_addr.s_addr),
-                        ntohs(clientAddress.sin_port));
+    // Set up the address struct for the server socket
+    setupAddressStruct(&serverAddress, atoi(argv[1]));
 
-   // Get the handshake message from the client and display it
-    char handshake_buf[1];
-    // Read the client's message from the socket
-    char charsRead = recv(connectionSocket, handshake_buf, 1, 0); 
-    if (charsRead < 0){
-      fprintf(stderr,"ERROR reading from socket");
+    // Associate the socket to the port
+    int binding_status = bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    if (binding_status < 0){
+        fprintf(stderr, RED "ERROR on binding");
+        exit(1);
     }
-    printf("SERVER: I received this from the client: \"%s\"\n", handshake_buf);
 
-    // Close the connection socket for this client
-    close(connectionSocket); 
-}
-    
-    // Close the listening socket
-    close(listenSocket); 
+    // Start listening for connetions. Allow up to 5 connections to queue up
+    listen(listenSocket, 5); 
 
-    return 0;
+    // Accept a connection, blocking if one is not available until one connects
+    while(1){
+        // Accept the connection request which creates a connection socket
+        connectionSocket = accept(listenSocket, 
+                    (struct sockaddr *)&clientAddress, 
+                    &sizeOfClientInfo); 
+        if (connectionSocket < 0){
+            error("ERROR on binding");
+        }
+
+        fprintf(stdout, WHITE "SERVER: Connected to client running at host %d port %d\n", 
+                            ntohs(clientAddress.sin_addr.s_addr),
+                            ntohs(clientAddress.sin_port));
+
+    // Get the handshake message from the client
+        char handshake_buf[2];
+        // Read the client's message from the socket
+        int charsRead = recv(connectionSocket, handshake_buf, 2, 0); 
+        if (charsRead < 0){
+            fprintf(stderr,"ERROR reading from socket");
+        }
+        fprintf(stdout, WHITE "SERVER: I received this from the client: \"%s\"\n", handshake_buf);
+
+        //verifies handshake message
+        //if messagematch, continue
+        if ( strcmp(handshake_buf,"?") == 0){
+            // Send a identifying message back to the client
+            int charsSent = send(connectionSocket, "@\0", 2, 0); 
+            if (charsSent < 0){
+                error("ERROR writing to socket");
+            }
+
+            // Get the textfile size from the client
+            char buff1[7];
+            // Read the client's message from the socket
+            int charsRead1 = recv(connectionSocket, buff1, 6, 0); 
+            buff1[charsRead1] = '\0';
+            if (charsRead1 < 0){
+                fprintf(stderr,"ERROR reading from socket");
+            }
+            fprintf(stdout, WHITE "SERVER: I received this from the client: \"%s\"\n", buff1);
+
+            // Send confirmation for receiving message back to the client
+            int charsSent1 = send(connectionSocket, "!\0", 2, 0); 
+            if (charsSent1 < 0){
+                error("ERROR writing to socket");
+            }
+
+            //converts textfile string size to number
+            int text_size = atoi(buff1);
+            memset(buff1,0,7);
+            //creates textfile buffer with text_size
+            char text_buffer[text_size+1];
+
+            //receives textfile from client
+            int charsRead2 = recv(connectionSocket, text_buffer, text_size, MSG_WAITALL); 
+            if (charsRead2 < 0){
+                fprintf(stderr,"ERROR reading from socket");
+            }
+            text_buffer[charsRead2] = '\0';
+            fprintf(stdout, WHITE "SERVER: I received this from the client: \"%s\"\n", text_buffer);
+
+            // Close the connection socket for this client
+            close(connectionSocket); 
+        }
+
+        //otherwise socket connection is closed
+        else{
+            fprintf(stderr, RED "Connected to wrong client\n");
+            close(connectionSocket);
+        }
+    }
+        
+        // Close the listening socket
+        close(listenSocket); 
+
+        return 0;
 }
